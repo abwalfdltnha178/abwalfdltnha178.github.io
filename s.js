3f0a0e53ffff5e3c2e034c9403b8511426dab6d3
ابوالفضل تنها <82462095+abwalfdltnha178@users.noreply.github.com>
2024-02-14 12:37:31 +0330

    Update s.js
    Signed-off-by: ابوالفضل تنها <82462095+abwalfdltnha178@users.noreply.github.com>

--- s.js
+++ s.js
@@ -1,3 +1,35 @@
+// ایجاد یک div برای صفحه لودینگ و اضافه کردن کلاس loading
+var loadingPage = document.createElement("div");
+loadingPage.className = "loading";
+loadingPage.style.display = "flex";
+loadingPage.style.justifyContent = "center";
+loadingPage.style.alignItems = "center";
+loadingPage.style.position = "fixed";
+loadingPage.style.zIndex = "9999";
+loadingPage.style.left = "0";
+loadingPage.style.top = "0";
+loadingPage.style.width = "100%";
+loadingPage.style.height = "100%";
+loadingPage.style.backgroundColor = "rgba(0,0,0,0.5)";
+
+// ایجاد یک div برای لوگو و اضافه کردن آن به صفحه لودینگ
+var logoDiv = document.createElement("div");
+logoDiv.style.backgroundImage = "url('/icon.svg')"; // تغییر 'path/to/your/logo.png' به مسیر تصویر لوگوی سایت شما
+logoDiv.style.width = "100px"; // تغییر به اندازه مورد نظر برای لوگو
+logoDiv.style.height = "100px"; // تغییر به اندازه مورد نظر برای لوگو
+logoDiv.style.backgroundRepeat = "no-repeat";
+logoDiv.style.backgroundPosition = "center";
+logoDiv.style.backgroundSize = "contain";
+loadingPage.appendChild(logoDiv);
+
+// اضافه کردن صفحه لودینگ به body
+document.body.appendChild(loadingPage);
+
+// پنهان کردن صفحه لودینگ وقتی تمام محتوا بارگیری شده است
+window.onload = function() {
+    loadingPage.style.display = "none";
+};
+
 function opn(){
 document.getElementsByTagName("nav")[0].classList.toggle("opn");
 document.getElementById("bg-menu").classList.toggle("bg-o");
@@ -50,4 +82,4 @@ function toggleTransition() {
     } else {
       element.style.transition = 'none';
     }
-  });
+  });		    
