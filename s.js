4c6826de7b917f25d768c04e0f7b7345e63f47a1
ابوالفضل تنها <82462095+abwalfdltnha178@users.noreply.github.com>
2024-04-04 12:45:36 +0330

    Update s.js
    Signed-off-by: ابوالفضل تنها <82462095+abwalfdltnha178@users.noreply.github.com>

--- s.js
+++ s.js
@@ -1,42 +1,42 @@
- document.addEventListener("DOMContentLoaded", function() {
-    var loadingPage = document.createElement("div");
-    loadingPage.className = "loading";
-    loadingPage.style.display = "flex";
-    loadingPage.style.justifyContent = "center";
-    loadingPage.style.alignItems = "center";
-    loadingPage.style.position = "fixed";
-    loadingPage.style.zIndex = "9999";
-    loadingPage.style.left = "0";
-    loadingPage.style.top = "0";
-    loadingPage.style.width = "100%";
-    loadingPage.style.height = "100%";
-    loadingPage.style.backgroundColor = "rgba(0,0,0,0.5)";
+document.addEventListener("DOMContentLoaded", function() {
+   var loadingPage = document.createElement("div");
+   loadingPage.className = "loading";
+   loadingPage.style.display = "flex";
+   loadingPage.style.justifyContent = "center";
+   loadingPage.style.alignItems = "center";
+   loadingPage.style.position = "fixed";
+   loadingPage.style.zIndex = "9999";
+   loadingPage.style.left = "0";
+   loadingPage.style.top = "0";
+   loadingPage.style.width = "100%";
+   loadingPage.style.height = "100%";
+   loadingPage.style.backgroundColor = "rgba(0,0,0,0.5)";
 
-    var logoDiv = document.createElement("div");
-    logoDiv.style.backgroundImage = "url('/icon.svg')"; 
-    logoDiv.style.width = "100px"; 
-    logoDiv.style.height = "100px"; 
-    logoDiv.style.backgroundRepeat = "no-repeat";
-    logoDiv.style.backgroundPosition = "center";
-    logoDiv.style.backgroundSize = "contain";
-    loadingPage.appendChild(logoDiv);
+   var logoDiv = document.createElement("div");
+   logoDiv.style.backgroundImage = "url('/icon.svg')"; 
+   logoDiv.style.width = "100px"; 
+   logoDiv.style.height = "100px"; 
+   logoDiv.style.backgroundRepeat = "no-repeat";
+   logoDiv.style.backgroundPosition = "center";
+   logoDiv.style.backgroundSize = "contain";
+   loadingPage.appendChild(logoDiv);
 
-    document.body.appendChild(loadingPage);
+   document.body.appendChild(loadingPage);
 
-    window.onload = function() {
-        loadingPage.style.display = "none";
-    };
+   window.onload = function() {
+       loadingPage.style.display = "none";
+   };
 });
 
 // توابع خارج از DOMContentLoaded
 
 function opn() {
-    document.getElementsByTagName("nav")[0].classList.toggle("opn");
-    document.getElementById("bg-menu").classList.toggle("bg-o");
+   document.getElementsByTagName("nav")[0].classList.toggle("opn");
+   document.getElementById("bg-menu").classList.toggle("bg-o");
 }
 
 function Dark(state) {
-    document.documentElement.classList.toggle("dark-mode", state);
+   document.documentElement.classList.toggle("dark-mode", state);
 }
 
 isDarkmode = window.matchMedia("(prefers-color-scheme:dark)");
@@ -44,53 +44,53 @@ Dark(isDarkmode.matches);
 isDarkmode.addListener((evt) => Dark(evt.matches));
 
 function ScrollTop() {
-    var Btn = document.getElementById("scroll");
-    var SvgHeader = document.getElementById("svgheader");
-    if (window.pageYOffset > 460) {
-        Btn.classList.add("show");
-        SvgHeader.classList.remove("svgheader");
-    } else {
-        Btn.classList.remove("show");
-        SvgHeader.classList.add("svgheader");
-    }
+   var Btn = document.getElementById("scroll");
+   var SvgHeader = document.getElementById("svgheader");
+   if (window.pageYOffset > 460) {
+       Btn.classList.add("show");
+       SvgHeader.classList.remove("svgheader");
+   } else {
+       Btn.classList.remove("show");
+       SvgHeader.classList.add("svgheader");
+   }
 }
 
 function toggleTransition() {
-    const checkbox = document.getElementById('toggleTransition');
-    const elements = document.querySelectorAll('*');
+   const checkbox = document.getElementById('toggleTransition');
+   const elements = document.querySelectorAll('*');
 
-    elements.forEach(element => {
-        const styles = getComputedStyle(element);
-        if (checkbox.checked) {
-            element.style.transition = "0.5s";
-        } else {
-            element.style.transition = 'none';
-        }
-    });
+   elements.forEach(element => {
+       const styles = getComputedStyle(element);
+       if (checkbox.checked) {
+           element.style.transition = "0.5s";
+       } else {
+           element.style.transition = 'none';
+       }
+   });
 }
 
 function updateThemeColor() {
-    var bgColor = window.getComputedStyle(document.body, null).getPropertyValue('background-color');
-    var metaTag = document.querySelector('meta[name="theme-color"]');
-    if (metaTag) {
-        metaTag.setAttribute('content', bgColor);
-    } else {
-        var newMetaTag = document.createElement('meta');
-        newMetaTag.name = "theme-color";
-        newMetaTag.content = bgColor;
-        document.getElementsByTagName('head')[0].appendChild(newMetaTag);
-    }
+   var bgColor = window.getComputedStyle(document.body, null).getPropertyValue('background-color');
+   var metaTag = document.querySelector('meta[name="theme-color"]');
+   if (metaTag) {
+       metaTag.setAttribute('content', bgColor);
+   } else {
+       var newMetaTag = document.createElement('meta');
+       newMetaTag.name = "theme-color";
+       newMetaTag.content = bgColor;
+       document.getElementsByTagName('head')[0].appendChild(newMetaTag);
+   }
 }
 
 document.addEventListener("DOMContentLoaded", updateThemeColor);
 
 var observer = new MutationObserver(function(mutationsList, observer) {
-    for(var mutation of mutationsList) {
-        if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
-            updateThemeColor();
-            break;
-        }
-    }
+   for(var mutation of mutationsList) {
+       if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+           updateThemeColor();
+           break;
+       }
+   }
 });
 
-observer.observe(document.body, { attributes: true, subtree: true });
+observer.observe(document.documentElement, { attributes: true, subtree: true });
