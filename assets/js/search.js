cc1583d7bd2e1ff013f8e2a818ff58aae4535d12
abwalfdltnha178 <abwalfdltnha178@users.noreply.github.com>
2024-11-15 12:20:02 +0000

    deploy: f24cee7eaf7c4ae906c6db53855e140a471d901c

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,40 +1,32 @@
-import { liteClient as algoliasearch } from 'algoliasearch/lite';
-import instantsearch from 'instantsearch.js';
-import { searchBox, hits, configure } from 'instantsearch.js/es/widgets';
-import { getPropertyByPath } from 'instantsearch.js/es/lib/utils';
-
-const searchClient = algoliasearch('N9TTQ38R4A', 'b6ac49385375e4237691c249390227c6');
+گconst searchClient = algoliasearch('N9TTQ38R4A', 'b6ac49385375e4237691c249390227c6');
 
 const search = instantsearch({
-  indexName: 'My First Application',
+  indexName: 'My First Application', // نام ایندکس شما در Algolia
   searchClient,
 });
 
 search.addWidgets([
-  searchBox({
-    container: "#searchbox"
+  // ویجت جعبه جستجو با placeholder
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+    placeholder: 'جستجو...',
   }),
-  configure({
-    hitsPerPage: 5
-  }),
-  hits({
-    container: "#hits",
+
+  // ویجت نتایج جستجو
+  instantsearch.widgets.hits({
+    container: '#hits',
     templates: {
-      item: (hit, { html, components }) => html`
-        <div>
-          <div class="hit-title">
-					  ${components.Highlight({ hit, attribute: "title" })}
-					</div>
-					<div class="hit-objectID">
-					  ${components.Highlight({ hit, attribute: "objectID" })}
-					</div>
-					<div class="hit-date">
-					  ${components.Highlight({ hit, attribute: "date" })}
-					</div>
-        </div>
-      `,
+      item(hit) {
+        return `
+          <div>
+            <h2>${hit.title}</h2>
+            <p>${hit.description}</p>
+          </div>
+        `;
+      },
     },
   }),
 ]);
 
-export default search;
+// شروع جستجو
+search.start();
