8a639c6f95c410b45faaa9d0cd9d7796d3c8683c
abwalfdltnha178 <abwalfdltnha178@users.noreply.github.com>
2025-06-01 14:27:24 +0000

    deploy: ee4b6a01cb7f153ca79872898c88bc56cc321ea8

---
+++ amp/assets/js/search.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function(){
+const searchClient = algoliasearch('N9TTQ38R4A', 'b6ac49385375e4237691c249390227c6');
+
+const search = instantsearch({
+  indexName: 'My First Application', // نام ایندکس شما در Algolia
+  searchClient,
+});
+
+search.addWidgets([
+  // ویجت جعبه جستجو با placeholder
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+    placeholder: 'جستجو...',
+  }),
+
+  // ویجت نتایج جستجو
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item(hit) {
+        return `
+          <div>
+            <h2>${hit.title}</h2>
+            <p>${hit.description}</p>
+          </div>
+        `;
+      },
+    },
+  }),
+]);
+
+// شروع جستجو
+search.start();
+})
