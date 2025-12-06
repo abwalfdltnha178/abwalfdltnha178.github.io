456ab15ac72e59466af5b7075f00b6925181151e
ابوالفضل تنها <82462095+abwalfdltnha178@users.noreply.github.com>
2025-12-06 21:28:49 +0330

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ export default defineConfig({
     shikiConfig: {
       theme: 'github-dark'
     }
-  }
+  },
+
+  // 👈 ویرگول اینجا باید باشد
   vite: {
     define: {
       'import.meta.env.PUBLIC_LAST_COMMIT_DATE': JSON.stringify(process.env.PUBLIC_LAST_COMMIT_DATE)
